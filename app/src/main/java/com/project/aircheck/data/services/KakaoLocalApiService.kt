9b39ad67d393bfc74254cd4b9abe7a20b0437db0
heymoko <gozila__adm@naver.com>
2022-04-27 17:42:39 +0900

    retrofit

---
+++ app/src/main/java/com/project/aircheck/data/services/KakaoLocalApiService.kt
@@ -0,0 +1,17 @@
+package com.project.aircheck.data.services
+
+import com.project.aircheck.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface KakaoLocalApiService {
+
+    @Headers("Authorization: KakaoAK ${BuildConfig.KAKAO_API_KEY}")
+    @GET("/v2/local/geo/transcoord.json?output_coord=TM")
+    suspend fun getTmCoordinates(
+        @Query("x") longitude: Double,
+        @Query("y") latitude: Double
+    )
+
+}
